9d106c1fd7bc11298b490b67a5396ee7c1afcb88
Omri Ovadia <omriovadia8@gmail.com>
2023-10-12 22:36:53 +0300

    Implement line draw order control

--- Assets/Scripts/LineFactory.cs
+++ Assets/Scripts/LineFactory.cs
@@ -1,19 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LineFactory : MonoBehaviour
 {
     public GameObject linePrefab;
     Line activeLine;
+    private int currentSortingOrder = 0;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             GameObject newLine = Instantiate(linePrefab);
             activeLine = newLine.GetComponent<Line>();
+            activeLine.GetComponent<LineRenderer>().sortingOrder = currentSortingOrder++; 
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -26,6 +25,5 @@ public class LineFactory : MonoBehaviour
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             activeLine.UpdateLine(mousePos);
         }
-        
     }
 }
